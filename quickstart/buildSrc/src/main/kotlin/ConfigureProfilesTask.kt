6e281327fe4d137a0b027276c203514321728b55
iggy-da <jan.otto@digitalasset.com>
2025-02-21 14:44:29 +1100

    typo

--- quickstart/buildSrc/src/main/kotlin/ConfigureProfilesTask.kt
+++ quickstart/buildSrc/src/main/kotlin/ConfigureProfilesTask.kt
@@ -1,4 +1,4 @@
-// Copyright (c) 2025, Digital Asset (Switzerland) GmbH and/or its affiliates
+// Copyright (c) 2025, Digital Asset (Switzerland) GmbH and/or its affiliates. All rights reserved.
 // SPDX-License-Identifier: 0BSD
 
 import org.gradle.api.DefaultTask
