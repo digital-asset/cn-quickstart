cfa330ad220f13d84503e866de3dc2c307bb00f5
Judy Wu <judy.wu@digitalasset.com>
2025-02-27 10:58:49 +0800

    update start-docker-desktop script to support different OS

--- quickstart/scripts/start-docker-desktop.sh
+++ quickstart/scripts/start-docker-desktop.sh
@@ -15,8 +15,27 @@ echo "Checking if Docker is running..."
 
 # Check if Docker is running
 if ! is_docker_running; then
+  OS_TYPE="$(uname)"
   echo "Docker is not running. Starting Docker Desktop..."
-  open -a Docker
+  case "$OS_TYPE" in
+  
+  Darwin)
+    # macOS
+    open -a Docker;;
+
+  Linux)
+    # Linux
+    sudo systemctl start docker;;
+
+  MINGW*|CYGWIN*|MSYS*)
+    # Windows
+    powershell.exe -Command "Start-Service docker";;
+  
+  *)
+    echo "Unsupported operating system detected: $OS_TYPE"
+    echo "Please start Docker manually on your system."
+    exit 1;;
+  esac
 
   # Wait until Docker is running or stop after 5 iterations
   max_attempts=5
@@ -27,6 +46,7 @@ if ! is_docker_running; then
     attempt=$((attempt + 1))
     if [ $attempt -ge $max_attempts ]; then
       echo "Error: Docker did not start within the expected time after $max_attempts attempts."
+      echo "Please start Docker manually on your system."
       exit 1
     fi
   done
