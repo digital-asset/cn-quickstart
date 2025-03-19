4dfb98afdd7b3e6b035887b830531c121b77b2da
iggy-da <jan.otto@digitalasset.com>
2025-03-19 17:46:55 +1100

    copyr header

--- quickstart/integration-test/tests/helpers.ts
+++ quickstart/integration-test/tests/helpers.ts
@@ -1,3 +1,5 @@
+// Copyright (c) 2025, Digital Asset (Switzerland) GmbH and/or its affiliates. All rights reserved.
+// SPDX-License-Identifier: 0BSD
 import {Page, expect} from '@playwright/test';
 
 /**
