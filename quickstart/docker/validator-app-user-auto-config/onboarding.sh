0a07970c8b5dfd6a38d156c754e29943792f3d96
Peter Kvokacka <peter.kvokacka@digitalasset.com>
2025-03-20 15:07:03 +1100

    canton components in one JVM

--- quickstart/docker/validator-app-user-auto-config/onboarding.sh
+++ quickstart/docker/validator-app-user-auto-config/onboarding.sh
@@ -9,17 +9,17 @@ source /app/utils.sh
 APP_USER_PARTICIPANT_ADMIN_TOKEN=$(get_admin_token $AUTH_APP_USER_VALIDATOR_CLIENT_SECRET $AUTH_APP_USER_VALIDATOR_CLIENT_ID $AUTH_APP_USER_TOKEN_URL)
 
 if [ ! -f /tmp/onboarding-dars-uploaded ]; then
-  upload_dars "$APP_USER_PARTICIPANT_ADMIN_TOKEN" "participant-app-provider:27575"
+  upload_dars "$APP_USER_PARTICIPANT_ADMIN_TOKEN" "participant:27575"
   touch /tmp/onboarding-dars-uploaded
 fi
 
-APP_USER_PARTY=$(get_user_party "$APP_USER_PARTICIPANT_ADMIN_TOKEN" $AUTH_APP_USER_VALIDATOR_USER_ID "participant-app-provider:27575")
+APP_USER_PARTY=$(get_user_party "$APP_USER_PARTICIPANT_ADMIN_TOKEN" $AUTH_APP_USER_VALIDATOR_USER_ID "participant:27575")
 
 # To update user name in metadata
-update_user "$APP_USER_PARTICIPANT_ADMIN_TOKEN" $AUTH_APP_USER_WALLET_ADMIN_USER_ID $AUTH_APP_USER_WALLET_ADMIN_USER_NAME $APP_USER_PARTY "participant-app-provider:27575"
-update_user "$APP_USER_PARTICIPANT_ADMIN_TOKEN" $AUTH_APP_USER_VALIDATOR_USER_ID $AUTH_APP_USER_VALIDATOR_USER_NAME $APP_USER_PARTY "participant-app-provider:27575"
+update_user "$APP_USER_PARTICIPANT_ADMIN_TOKEN" $AUTH_APP_USER_WALLET_ADMIN_USER_ID $AUTH_APP_USER_WALLET_ADMIN_USER_NAME $APP_USER_PARTY "participant:27575"
+update_user "$APP_USER_PARTICIPANT_ADMIN_TOKEN" $AUTH_APP_USER_VALIDATOR_USER_ID $AUTH_APP_USER_VALIDATOR_USER_NAME $APP_USER_PARTY "participant:27575"
 
 # needed for create-app-install-request
-grant_rights "$APP_USER_PARTICIPANT_ADMIN_TOKEN" $AUTH_APP_USER_WALLET_ADMIN_USER_ID $APP_USER_PARTY "ReadAs ActAs" "participant-app-provider:27575"
+grant_rights "$APP_USER_PARTICIPANT_ADMIN_TOKEN" $AUTH_APP_USER_WALLET_ADMIN_USER_ID $APP_USER_PARTY "ReadAs ActAs" "participant:27575"
 
-delete_user "$APP_USER_PARTICIPANT_ADMIN_TOKEN" participant_admin "participant-app-provider:27575"
+delete_user "$APP_USER_PARTICIPANT_ADMIN_TOKEN" participant_admin "participant:27575"
