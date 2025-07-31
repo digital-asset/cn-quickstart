81fef905513c4b88bd2c70a33e42d49936d59bd0
Peter Garmaz <peter.garmaz@digitalasset.com>
2025-07-31 15:08:21 +1200

    move nix setup from npins to nixflake - 25.05 lacks nodejs_18 so have updated to 20

---
+++ nix/shell.nix
@@ -0,0 +1,38 @@
+{ pkgs, ci }:
+let
+  inherit (pkgs) stdenv;
+  requiredPackages = with pkgs; ([
+    # these packages are required both in CI and for local development
+    bashInteractive
+    gnumake
+    jdk21
+    # nodejs_18
+    nodejs_20
+    typescript
+  ] ++ (if ci then [
+    # these packages should only be installed on CI
+  ] else [
+    # these packages are only installed on developer machines locally
+    circleci-cli
+  ])) ++ (lib.optionals stdenv.isDarwin [
+          pkgs.libiconv
+          pkgs.darwin.apple_sdk.frameworks.Security
+          ]);
+in
+pkgs.mkShell {
+  packages = requiredPackages;
+  LC_ALL = if stdenv.isDarwin then "" else "C.UTF-8";
+  env = {
+  };
+
+  shellHook = ''
+    export JAVA_HOME="$(readlink -e $(type -p javac) | sed  -e 's/\/bin\/javac//g')"
+
+    # there is a nix bug that the directory deleted by _nix_shell_clean_tmpdir can be the same as the general $TEMPDIR
+    eval "$(declare -f _nix_shell_clean_tmpdir | sed 's/_nix_shell_clean_tmpdir/orig__nix_shell_clean_tmpdir/')"
+    _nix_shell_clean_tmpdir() {
+        orig__nix_shell_clean_tmpdir "$@"
+        mkdir -p "$TEMPDIR" # ensure system TEMPDIR still exists
+    }
+    '';
+}
