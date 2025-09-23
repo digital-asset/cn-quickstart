3a46d78aa92ef3cbc9832c6ebc3dab9af8c529fd
Erwin Ramirez <erwin.ramirez@digitalasset.com>
2025-09-23 19:09:56 +1000

    fixed bootstrap loading separate logout and username out of bootstrap collapse tool

--- quickstart/frontend/src/components/Header.tsx
+++ quickstart/frontend/src/components/Header.tsx
@@ -13,20 +13,25 @@ const Header: React.FC = () => {
                     <a className="navbar-brand" href="#">
                         Canton Network Quickstart
                     </a>
-                    <button
-                        className="navbar-toggler"
-                        type="button"
-                        data-bs-toggle="collapse"
-                        data-bs-target="#navbarNav"
-                        aria-controls="navbarNav"
-                        aria-expanded="false"
-                        aria-label="Toggle navigation"
-                    >
-                        <span className="navbar-toggler-icon"></span>
-                    </button>
+                    <div>
+                        <button
+                            className="navbar-toggler"
+                            type="button"
+                            data-bs-toggle="collapse"
+                            data-bs-target="#navbarNav"
+                            aria-controls="navbarNav"
+                            aria-expanded="false"
+                            aria-label="Toggle navigation"
+                        >
+                            <span className="navbar-toggler-icon"></span>
+                        </button>
+                    </div>
                     <div className="collapse navbar-collapse" id="navbarNav">
                         <AuthenticatedLinks/>
                     </div>
+                </div>
+                <div>
+                    <UserSection />
                 </div>
             </nav>
         </header>
@@ -34,68 +39,70 @@ const Header: React.FC = () => {
 };
 
 const AuthenticatedLinks: React.FC = () => {
+    const {user, loading, fetchUser} = useUserStore();
+
+    React.useEffect(() => {
+        fetchUser();
+    }, [fetchUser]);
+
+    if (loading || user === null) {
+        return null;
+    }
+
+    return (
+        <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+            <li className="nav-item">
+                <Link className="nav-link" to="/">Home</Link>
+            </li>
+            <li className="nav-item">
+                <Link className="nav-link" to="/app-installs">AppInstalls</Link>
+            </li>
+            <li className="nav-item">
+                <Link className="nav-link" to="/licenses">Licenses</Link>
+            </li>
+            {user.isAdmin && (
+                <li className="nav-item">
+                    <Link className="nav-link" to="/tenants">Tenants</Link>
+                </li>
+            )}
+        </ul>
+    );
+};
+
+const UserSection: React.FC = () => {
     const {user, loading, fetchUser, logout} = useUserStore();
 
     React.useEffect(() => {
         fetchUser();
     }, [fetchUser]);
 
-    if (loading) {
-        return <div>Loading...</div>;
-    }
+    if (loading) return <div className="ms-auto">Loading...</div>;
+
 
     if (user === null) {
         return (
-            <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
+            <ul className="navbar-nav ms-auto">
                 <li className="nav-item">
-                    <Link className="nav-link" to="/login">
-                        Login
-                    </Link>
+                    <Link className="nav-link" to="/login">Login</Link>
                 </li>
             </ul>
         );
-    } else {
-        return (
-            <>
-                <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                    <li className="nav-item">
-                        <Link className="nav-link" to="/">
-                            Home
-                        </Link>
-                    </li>
-                    <li className="nav-item">
-                        <Link className="nav-link" to="/app-installs">
-                            AppInstalls
-                        </Link>
-                    </li>
-                    <li className="nav-item">
-                        <Link className="nav-link" to="/licenses">
-                            Licenses
-                        </Link>
-                    </li>
-                    {user.isAdmin && (
-                        <li className="nav-item">
-                            <Link className="nav-link" to="/tenants">
-                                Tenants
-                            </Link>
-                        </li>
-                    )}
-                </ul>
-                <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
-                    <li className="nav-item">
-                        <span className="nav-link" style={{fontWeight: 'bold'}} id="user-name">
-                            {user.name}
-                        </span>
-                    </li>
-                    <li className="nav-item">
-                        <button className="nav-link btn btn-link" onClick={logout}>
-                            Logout
-                        </button>
-                    </li>
-                </ul>
-            </>
-        );
     }
+
+    return (
+        <ul className="navbar-nav ms-auto">
+            <li className="nav-item">
+                <span className="nav-link fw-bold" id="user-name">
+                    {user.name}
+                </span>
+            </li>
+            <li className="nav-item">
+                <button className="nav-link btn btn-link" onClick={logout}>
+                    Logout
+                </button>
+            </li>
+        </ul>
+    );
 };
 
 export default Header;
