f6ee6e5da7123b17c721d35b150196858ebf5e1e
sammy-da <sammy.abed@digitalasset.com>
2026-01-23 11:20:18 -0500

    sdk 3.4.10, scribe 0.6.15 (#316)
    * sdk 3.4.10
    
    * bump scribe

--- quickstart/daml/build.gradle.kts
+++ quickstart/daml/build.gradle.kts
@@ -61,7 +61,7 @@ fun computeSdkVariables(): Map<String, Any> {
     }
 
     val damlSdkRuntimeVersion = VersionFiles.dotenv["DAML_RUNTIME_VERSION"] as String
-    val sdkVersion = "3.4.9"
+    val sdkVersion = "3.4.10"
     val damlSdkVersion = VersionFiles.damlYamlSdk
     val sdkArchive = "daml-sdk-$damlSdkRuntimeVersion-$sdkOs.tar.gz"
     val sdkUrl = "https://github.com/digital-asset/daml/releases/download/v${sdkVersion}/${sdkArchive}"
