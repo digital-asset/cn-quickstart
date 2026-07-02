a39e0a60726a93a7368ad5bec3d38a0d59bc44fe
Brian Healey <brian.healey@digitalasset.com>
2026-07-02 18:35:36 -0400

    update to sdk bundle 3.5.2

--- nix/dpm.nix
+++ nix/dpm.nix
@@ -1,15 +1,15 @@
 { pkgs ? import <nixpkgs> {} }:
 
 let
-  dpmVersion = "3.4.11";
+  dpmVersion = "3.5.2";
   system = pkgs.stdenv.hostPlatform.system;
   os = if pkgs.stdenv.isDarwin then "darwin" else "linux";
   arch = if pkgs.stdenv.isAarch64 then "arm64" else "amd64";
 
   dpmHashes = {
-    "x86_64-linux" = "sha256:1k9csx67s81jzr02qaa6ggh1hps4m1rd3yrpxrp1ly5mv2w2hwqp";
-    "aarch64-linux" = "sha256:162nasjhrh8vhn2j3hqswi8w7q1xnn7njcqzdkfk9aq8x1gq9lgw";
-    "aarch64-darwin" = "sha256:1i10yqf3a2i5fkhyhkk3dw35jbjdpxp236wdjz31d9z5xz80zg5w";
+    "x86_64-linux" = "sha256:015npjj4npryg4c7k98g64zlnh4bx4jrwvbkvnhwsj7iniyfjr8c";
+    "aarch64-linux" = "sha256:0f9xbpbcl58ml8mfls4wgqc0gcnfkvivjxd20xhfd1lgls8nmbhq";
+    "aarch64-darwin" = "sha256:1srqpwglzljk0yiszb90bkp1b368a08dl95j77j72ijwa6hv2igr";
   };
   dpmHash = dpmHashes.${system} or (throw "Unsupported system: ${system}");
 in
